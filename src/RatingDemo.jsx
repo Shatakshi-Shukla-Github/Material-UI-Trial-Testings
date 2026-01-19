c2e76cd84816f1024aea46476395e1bd3feb0ff0
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-01-19 23:07:56 +0530

    Implementing the Input and Slider Components

--- src/RatingDemo.jsx
+++ src/RatingDemo.jsx
@@ -1,7 +1,13 @@
 import Rating from "@mui/material/Rating";
 import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Slider from "@mui/material/Slider";
 export default function RatingDemo() {
   const [score, setScore] = useState(3);
+  const [volume, setVolume] = useState(22);
+  const changeVolume = (e) => {
+    setVolume(e.target.value);
+  };
   return (
     <div>
       <h1>Rating is:- {score}</h1>
@@ -12,6 +18,15 @@ export default function RatingDemo() {
           setScore(newValue);
         }}
       />
+
+      <TextField
+        id="outlined-basic"
+        label="Cato Name"
+        variant="outlined"
+        placeholder="Mews"
+      />
+      <h2>{}</h2>
+      <Slider aria-label="Volume" value={volume} onChange={changeVolume} />
     </div>
   );
 }
