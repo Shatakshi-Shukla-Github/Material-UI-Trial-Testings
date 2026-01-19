43682c6b728ade3c42c148f85b5a29112e968486
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-01-19 21:18:14 +0530

    Trying Material-UI components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import Fingerprint from "@mui/icons-material/Fingerprint";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button color="success" variant="contained" size="small">
+        Contained
+      </Button>
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+      <IconButton aria-label="fingerprint" color="success">
+        <Fingerprint />
+      </IconButton>
+    </>
+  );
+}
+
+export default App;
